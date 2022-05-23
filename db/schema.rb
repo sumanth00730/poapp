79342c23195ef0c5d4e8ce8e61e179f7b30c5560
sumanth <sumanthmahilamuri007@gmail.com>
2022-05-23 21:08:51 +0530

    cors updated

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_165704) do
     t.float "total_amount"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
